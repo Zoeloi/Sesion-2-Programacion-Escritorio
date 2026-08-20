ead62243405db421c2528b174b9c4a24c1c8399a
Zoeloi <eaalmendarez@uamv.edu.ni>
2026-08-19 19:29:03 -0600

    listo

--- PROYECTO 1/PROYECTO 1/src/main/java/com/example/proyecto1/HelloApplication.java
+++ PROYECTO 1/PROYECTO 1/src/main/java/com/example/proyecto1/HelloApplication.java
@@ -3,8 +3,12 @@ package com.example.proyecto1;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -19,12 +23,75 @@ public class HelloApplication extends Application {
         root.setPadding (new Insets(20));
 
         Label lblTitle = new Label ("Calculadora Basica");
-        root.getChildren ().add(lblTitle);
+        Label lblNumber1 = new Label("Numero1");
+        TextField tfNumber1 = new TextField();
+        Label lblNumber2 = new Label("Numero 2");
+        TextField tfNumber2 = new TextField();
+        Label lblAnswer = new Label("Resultado= 0");
+
+
+
+
+
+        Button btnSum = new Button ("+");
+        Button btnSub = new Button ("-");
+        Button btnMul = new Button ("*");
+        Button btnDiv = new Button ("/");
+
+
+        btnSum.setOnAction ( e-> {
+            int num1 = Integer.parseInt(tfNumber1.getText());
+            int num2 = Integer.parseInt(tfNumber2.getText());
+            int resultado = HelloController.sum(num1,num2);
+            lblAnswer.setText(String.valueOf(resultado));
+        });
+
+        btnSub.setOnAction(e -> {
+            int num1 = Integer.parseInt(tfNumber1.getText());
+            int num2 = Integer.parseInt(tfNumber2.getText());
+            int resultado = HelloController.sub(num1, num2);
+            lblAnswer.setText(String.valueOf(resultado));
+        });
+
+        btnMul.setOnAction(e -> {
+            int num1 = Integer.parseInt(tfNumber1.getText());
+            int num2 = Integer.parseInt(tfNumber2.getText());
+            int resultado = HelloController.mul(num1, num2);
+            lblAnswer.setText(String.valueOf(resultado));
+        });
+
+        btnDiv.setOnAction(e -> {
+            int num1 = Integer.parseInt(tfNumber1.getText());
+            int num2 = Integer.parseInt(tfNumber2.getText());
+
+            if (num2 != 0) {
+                int resultado = HelloController.div(num1, num2);
+                lblAnswer.setText(String.valueOf(resultado));
+            } else {
+                lblAnswer.setText("Error: división por cero");
+            }
+        });
+
+        HBox buttons = new HBox();
+        buttons.setSpacing(10);
+        buttons.setPadding((new Insets(5)));
+        buttons.setAlignment(Pos.CENTER);
+        buttons.setStyle("-fx-border-radius: 10; -fx-border-color: #ab0036;");
+        buttons.getChildren().addAll(btnSum,btnSub,btnMul,btnDiv);
+
+
+
+
+        root.getChildren().addAll(lblTitle,lblNumber1,tfNumber1,lblNumber2,tfNumber2,lblAnswer, buttons);
+
 
 
         Scene scene = new Scene (root);
         stage.setScene(scene);
         stage.setTitle("Ejemplo 1 - Seccion 2");
         stage.show();
+
+
+
     }
 }
--- PROYECTO 1/PROYECTO 1/src/main/java/com/example/proyecto1/HelloController.java
+++ PROYECTO 1/PROYECTO 1/src/main/java/com/example/proyecto1/HelloController.java
@@ -4,11 +4,20 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
-
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+  public static int sum (int n1, int n2){
+      return n1+n2;
+  }
+    public static int sub (int n1, int n2){
+        return n1-n2;
     }
+    public static int mul (int n1, int n2){
+        return n1*n2;
+    }
+    public static int div  (int n1, int n2){
+        if (n2 != 0){
+            return n1/n2;
+        }
+        return 0;
+    }
+
 }
